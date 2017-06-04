d734e8fb3d812e7669451e1239d54c90f71db1a1
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2017-06-03 20:58:22 -0400

    token test helper

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const ServiceTestHelper = require('./libs/serviceTestHelper.js').ServiceTestHelper;
+const TokenTestHelper = require('./libs/tokenTestHelper.js').TokenTestHelper;
 const Assert = require('./libs/serviceTestHelper.js').Assert;
 
 module.exports.ServiceTestHelper = ServiceTestHelper;
