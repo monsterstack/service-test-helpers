3dd24dd17bdd3186a1b9b710d39ab1d3d57e71b6
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2017-06-03 20:55:38 -0400

    jsonwebtoken helper

---
+++ libs/tokenTestHelper.js
@@ -0,0 +1,15 @@
+'use strict';
+const Promise = require('promise');
+const jwt = require('jsonwebtoken');
+
+class TokenTestHelper {
+	decodeSecret(secret, apiSecret) {
+		let p = new Promise((resolve, reject) => {
+			jwt.verify(secret, apiSecret, (err, decoded) => {
+				if (err) reject(err);
+				else resolve(decoded);
+			});
+		});
+		return p;
+	}
+}
