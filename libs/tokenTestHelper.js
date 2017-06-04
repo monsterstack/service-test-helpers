6f1b4fa2e7a82c800338fb6852aa5b1e95f33188
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2017-06-03 21:10:01 -0400

    jwt verify

--- libs/tokenTestHelper.js
+++ libs/tokenTestHelper.js
@@ -3,14 +3,8 @@ const Promise = require('promise');
 const jwt = require('jsonwebtoken');
 
 class TokenTestHelper {
-	decodeSecret(secret, apiSecret) {
-		let p = new Promise((resolve, reject) => {
-			jwt.verify(secret, apiSecret, (err, decoded) => {
-				if (err) reject(err);
-				else resolve(decoded);
-			});
-		});
-		return p;
+	decodeSecret(key, apiSecret) {
+		return jwt.verify(key, apiSecret);
 	}
 }
 
