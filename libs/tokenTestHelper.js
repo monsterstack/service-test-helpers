f66763bf6e86f2b3f2845587a5f072421eadcdc4
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2017-06-08 22:12:22 -0400

    code secret

--- libs/tokenTestHelper.js
+++ libs/tokenTestHelper.js
@@ -6,6 +6,13 @@ class TokenTestHelper {
 	decodeSecret(key, apiSecret) {
 		return jwt.verify(apiSecret, key);
 	}
+
+	codeSecret(tenant) {
+		return jwt.sign({
+			id: tenant.id,
+			name: tenant.name
+		}, tenant.apiKey);
+	}
 }
 
 module.exports.TokenTestHelper = TokenTestHelper;
